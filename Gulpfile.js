a3922adf0c29f560a2774ace6f651e69bc4dec2d
Felipe Rodrigues <fr.oliveira94@gmail.com>
2016-05-11 19:23:02 -0300

    header created

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./public/sass/**/*.sass')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('./public/sass/style.sass')
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./public/stylesheets'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./public/sass/**/*.sass', ['sass']);
+  gulp.watch('./public/sass/style.sass', ['sass']);
 });
